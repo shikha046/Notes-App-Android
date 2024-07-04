31265694c7eb1a51d496fedff721cdfdb395e3a0
Shikha <101914410+shikha046@users.noreply.github.com>
2024-07-05 05:00:17 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,82 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
+android {
+    namespace = "com.example.notesappmvvmkotlin"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.notesappmvvmkotlin"
+        minSdk = 26 // Set to a reasonable minimum SDK version
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    lintOptions {
+        disable("UnusedParameter")
     }
 }
 
-// You can uncomment this if you need to add repositories to all projects
-// allprojects {
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-// }
+dependencies {
+    implementation("androidx.core:core-ktx:1.10.1") // Ensure this is the latest stable version
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation Components
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    // Room Database
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
+
+    // Lifecycle components
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.8.0")
+    implementation("androidx.activity:activity-ktx:1.3.1")
+    implementation("androidx.fragment:fragment-ktx:1.7.1")
+
+    // Kotlin components
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+
+    compileOnly("com.android.tools.lint:lint-api:30.1.2")
+    compileOnly("com.android.tools.lint:lint-checks:30.1.2")
+}
